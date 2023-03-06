de206ffd970b074dff17de47d970712e59ccfc15
Evgeniy <Lymilight@gmail.com>
2023-03-06 21:01:46 +0700

    ex 004 div

---
+++ exampless/Exam_04/Program.cs
@@ -0,0 +1,9 @@
+﻿   // Пример№4
+// int numberA = 10;
+// int numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+   // Пример№4/1
+// double numberA = 12;
+// double numberB = 5;
+// Console.WriteLine(numberA / numberB);
