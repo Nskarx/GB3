0221a0cd68fe4084963de2846edc50085286675c
Evgeniy <Lymilight@gmail.com>
2023-03-06 21:05:36 +0700

    ex 006  cycle

---
+++ exampless/Exam_06/Program.cs
@@ -0,0 +1,40 @@
+﻿// Синтакси: начало
+// if(условие)
+// {
+//     набор действий 1
+// }
+// else
+// {
+//     набор действий 2
+// }
+
+         // Пример№6/1
+// // Console.Write("Введите имя пользователя: ");
+// // string username = Console.ReadLine();
+
+// // if(username == "Julia")
+// // {
+// //       Console.WriteLine("Ура, это же julia: ");
+// // }
+// // else
+// // {
+// //     Console.Write("Hello!: ");
+//     Console.WriteLine(username);
+// }
+
+
+         // Пример№6/2
+
+// Console.Write("Введите имя пользователя: ");
+// string username = Console.ReadLine();
+
+// if(username.ToLower() == "Julia")
+// {
+//     Console.WriteLine("Ура, это же julia: ");
+// }
+    
+// else
+// {
+//     Console.Write("Hello!: ");
+//     Console.WriteLine(username);
+// }
