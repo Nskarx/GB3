2e665a24bbb7690c15420242dd0a080365c1ac63
Evgeniy <Lymilight@gmail.com>
2023-03-06 20:53:49 +0700

    ex 003 sum

---
+++ exampless/Exam_03/Program.cs
@@ -0,0 +1,10 @@
+﻿//Написать программу сложения двух чисел
+//  int numberA = 3;
+//  int numberB = 3;
+//  Console.WriteLine(numberA + numberB);
+               //
+int numberA = 5;
+int numberB = 33;
+ int  result = numberA + numberB;
+ Console.WriteLine(result);
+
