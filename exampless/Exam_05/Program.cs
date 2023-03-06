865f63f6a865985f6c25a856f255775f6c2ed52f
Evgeniy <Lymilight@gmail.com>
2023-03-06 21:04:00 +0700

    ex 005 drandom

---
+++ exampless/Exam_05/Program.cs
@@ -0,0 +1,19 @@
+﻿//    // Пример№5 Случайности
+//    new  Random().Next("min , max -1");
+//    даст случайное целое число от min до max-1 [ min, max -1] или [min,max )
+
+// Пример№5
+
+//  int numberA = new Random().Next(1 , 10); // 1 2 3 4 5....9
+//  int numberB = new Random().Next(1 , 10);
+//  int result = numberA + numberB;
+//  Console.WriteLine(result);
+
+// Пример№5/1
+
+//   int numberA = new Random().Next(1 , 10); // 1 2 3 4 5....9
+//   Console.WriteLine(numberA);
+//  int numberB = new Random().Next(1 , 10);
+//    Console.WriteLine(numberB);
+//  int result = numberA + numberB;
+//  Console.WriteLine(result);
