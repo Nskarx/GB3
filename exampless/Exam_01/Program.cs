a23cbe2617263ea75bb8e5c84e5549fcb8645507
Evgeniy <Lymilight@gmail.com>
2023-03-19 19:00:12 +0700

    exam _09

--- exampless/Exam_01/Program.cs
+++ exampless/Exam_01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, 12World!");
+﻿//Console.ReadLine();с помощью нее происходит считывание данных
+// string (строка) -это тип данных и место где хвариться информация
+//username-имя переменной
+// = (Оператор) означает- возьми то что справа и положи в то что слева
+// Console.WriteLine("Введите ваше имя");
+// string username = Console.ReadLine();
+// Console.WriteLine("привет,");
+// Console.WriteLine(username);
+
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("привет,");
+Console.Write(username);
